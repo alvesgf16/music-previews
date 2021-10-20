d7938ebef5eb98475490991a6fa5dabe3fc1c3d9
alvesgf16 <alvesgabriel1994@gmail.com>
2021-10-20 12:12:24 -0300

    Adiciona lista de albums de artista existente

---
+++ src/components/AlbumCard.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default class AlbumCard extends Component {
+  render() {
+    const { album: {
+      artistName,
+      collectionId,
+      collectionName,
+      artworkUrl100 },
+    } = this.props;
+    return (
+      <div>
+        <Link
+          to={ `/album:${collectionId}` }
+          data-testid={ `link-to-album-${collectionId}` }
+        >
+          <img src={ artworkUrl100 } alt={ `${collectionName} cover` } />
+          <h5>{ collectionName }</h5>
+          <h6>{ artistName }</h6>
+        </Link>
+      </div>
+    );
+  }
+}
+
+AlbumCard.propTypes = {
+  album: PropTypes.shape({
+    artistName: PropTypes.string,
+    collectionId: PropTypes.number,
+    collectionName: PropTypes.string,
+    artworkUrl100: PropTypes.string,
+  }).isRequired,
+};
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import AlbumCard from '../components/AlbumCard';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
 import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 export default class Search extends Component {
   constructor() {
     super();
     this.state = {
-      artist: '',
+      input: '',
       isSearchButtonDisabled: true,
+      loading: false,
+      albums: [],
+      artist: '',
     };
 
     this.onInputChange = this.onInputChange.bind(this);
     this.enableSearchButton = this.enableSearchButton.bind(this);
     this.onSearchButtonClick = this.onSearchButtonClick.bind(this);
+    this.searchAlbums = this.searchAlbums.bind(this);
   }
 
   onInputChange({ target }) {
@@ -21,41 +27,61 @@ export default class Search extends Component {
     this.enableSearchButton();
   }
 
-  async onSearchButtonClick() {
-    const { artist } = this.state;
+  onSearchButtonClick() {
+    const { input } = this.state;
+    this.setState({ artist: input }, () => {
+      const { artist } = this.state;
+      this.searchAlbums(artist);
+      this.setState({ input: '' });
+    });
+  }
+
+  async searchAlbums(artist) {
+    this.setState({ loading: true, albums: [] });
     const response = await searchAlbumsAPI(artist);
-    console.log(response);
+    this.setState({ loading: false, albums: response });
   }
 
   enableSearchButton() {
-    this.setState(({ artist }) => {
+    this.setState(({ input }) => {
       const minInputLength = 2;
-      return ({ isSearchButtonDisabled: artist.length < minInputLength });
+      return ({ isSearchButtonDisabled: input.length < minInputLength });
     });
   }
 
   render() {
-    const { artist, isSearchButtonDisabled } = this.state;
+    const { input, isSearchButtonDisabled, loading, albums, artist } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <div data-testid="page-artist">
-          <input
-            name="artist"
-            value={ artist }
-            type="text"
-            data-testid="search-artist-input"
-            onChange={ this.onInputChange }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isSearchButtonDisabled }
-            onClick={ this.onSearchButtonClick }
-          >
-            Pesquisar
-          </button>
-        </div>
+        { loading ? <Loading /> : (
+          <div>
+            <input
+              name="input"
+              value={ input }
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.onInputChange }
+            />
+            <button
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ isSearchButtonDisabled }
+              onClick={ this.onSearchButtonClick }
+            >
+              Pesquisar
+            </button>
+          </div>
+        ) }
+        { albums.length > 0 && (
+          <div>
+            <h3>{`Resultado de álbuns de: ${artist}`}</h3>
+            { albums.map((album) => (
+              <AlbumCard key={ album.collectionId } album={ album } />
+            ))}
+          </div>
+        )}
       </div>
     );
   }
